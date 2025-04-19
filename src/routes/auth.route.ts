57725d4e01722453298738b3a49b8914b2927a1b
Sumit <sumitsunchu@gmail.com>
2025-04-19 00:14:24 -0700

    Changes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -10,4 +10,5 @@ router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/me", authMiddleware, authController.getCurrentUser);
 
+
 export default router;
